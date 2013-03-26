e77f48ea61d948cec8ef5853b7ce006962486831
Jerome Lecomte <jerle76@gmail.com>
2013-03-25 22:20:28 -0400

    implement initial state
    the testcases are now passing as intended

--- app/scripts/Simon.js
+++ app/scripts/Simon.js
@@ -1,5 +1,10 @@
 define([], function() {
   var Simon = function Simon() {
+    this.sequence = [];
+  };
+
+  Simon.prototype.getRoundNo = function() {
+    return this.sequence.length;
   };
 
   return Simon;
