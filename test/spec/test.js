fb40f900eaf8d19c8ff1e3ffac97cf21f9e8e1a1
Jerome Lecomte <jerle76@gmail.com>
2013-03-25 21:32:56 -0400

    load testcases through requirejs
    also:
    - don't bail on 1st error
    - load expect, should and assert libraries

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,9 @@
-/*global describe it */
-'use strict';
-(function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+require.config({
+  paths: {
+  }
+});
 
-            });
-        });
-    });
-})();
+require([], function() {
+  mocha.run();
+});
+
