af20521fbcd380fdc929389fd945a18b0fbfcc6f
Jerome Lecomte <jerle76@gmail.com>
2013-03-26 21:56:37 -0400

    implement random color picking
    the test cases are now passing as intended.
    
    the game design doesn't state anything about fairness in the selection process
    and I'm not going to test for that because it's not important to me. I trust
    Math.random() to be fair anyway. What matters to me is that Simon only adds
    valid colors to his sequence.
    
    the goal BDD isn't to achieve 100% coverage of your codebase. you should only
    test what is important to you.

--- app/scripts/Simon.js
+++ app/scripts/Simon.js
@@ -8,6 +8,17 @@ define([], function() {
   };
 
   Simon.prototype.pickRandomColor = function() {
+    var odd = Math.random();
+
+    if (odd < 0.25) {
+      return 'blue';
+    } else if (odd < 0.5) {
+      return 'red';
+    } else if (odd < 0.75) {
+      return 'green';
+    } else {
+      return 'yellow';
+    }
   };
 
   return Simon;
