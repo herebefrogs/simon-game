6f67bf7be9fa31d80abbce3cecf0323d73cdfc78
Jerome Lecomte <jerle76@gmail.com>
2013-04-12 17:01:14 -0400

    add test case for Simon replaying the next color in his sequence

--- test/spec/simon.js
+++ test/spec/simon.js
@@ -14,40 +14,85 @@ define(['app/Simon', 'lib/sinon-1.6.0'], function (Simon) {
   });
 
   describe("On Simon's turn", function() {
-    var game = page = null;
+    describe("When Simon's turn starts", function() {
+      var game = page = null;
 
-    before(function() {
-      // arrange
-      page = $('<div>');
-      page.append('<span id="color-count"></span>');
+      before(function() {
+        // arrange
+        page = $('<div>');
+        page.append('<span id="color-count"></span>');
+        page.append('<div id="blue"></div>' +
+                    '<div id="red"></div>' +
+                    '<div id="green"></div>' +
+                    '<div id="yellow"></div>');
 
-      game = new Simon(page);
-      game.pickRandomColor = sinon.spy(game, 'pickRandomColor');
-      game.replayColorAt = sinon.spy();
+        game = new Simon(page);
+        game.pickRandomColor = sinon.spy(game, 'pickRandomColor');
+        game.replayColorAt = sinon.spy(game, 'replayColorAt');
 
-      // act
-      game.newTurn();
+        // act
+        game.newTurn();
+      });
+
+      // assert
+      it('Simon should pick a color randomly', function() {
+        game.pickRandomColor.callCount.should.equal(1);
+      });
+
+      it("Simon should add this color at the end of his sequence", function() {
+        game.sequence.should.have.length(1);
+      });
+
+      it('the new color Simon added to his sequence should be either blue, red, green or yellow', function() {
+        [ 'blue', 'red', 'yellow', 'green' ].should.include(game.sequence[0]);
+      });
+
+      it("the page should update the number of colors in Simon's sequence", function() {
+        page.find('#color-count').text().should.equal('1');
+      });
+
+      it('Simon should start replaying his color sequence from the beginning', function() {
+        game.replayColorAt.firstCall.args[0].should.equal(0);
+      });
+
+      it('Simon should animate the tile matching the new color', function() {
+        page.find('#' + game.sequence[0]).hasClass('flash').should.equal(true);
+      });
     });
 
-    // assert
-    it('Simon should pick a color randomly', function() {
-      game.pickRandomColor.callCount.should.equal(1);
-    });
+    describe('When the sequence plays back', function() {
+      var game = page = clock = null;
 
-    it("Simon should add this color at the end of his sequence", function() {
-      game.sequence.should.have.length(1);
-    });
+      before(function() {
+        // arrange
+        page = $('<div>')
+        page.append('<div id="blue" class="flash"></div>' +
+                    '<div id="red"></div>' +
+                    '<div id="green"></div>' +
+                    '<div id="yellow"></div>');
 
-    it('the new color Simon added to his sequence should be either blue, red, green or yellow', function() {
-      [ 'blue', 'red', 'yellow', 'green' ].should.include(game.sequence[0]);
-    });
+        game = new Simon(page);
+        game.sequence = ['blue', 'red'];
+        game.replayColorAt = sinon.spy(game, 'replayColorAt');
+        clock = sinon.useFakeTimers();
 
-    it("the page should update the number of colors in Simon's sequence", function() {
-      page.find('#color-count').text().should.equal('1');
-    });
+        // act
+        game.replayColorAt(0);
+        clock.tick(1250);
+      });
 
-    it('Simon should start replaying his color sequence from the beginning', function() {
-      game.replayColorAt.firstCall.args[0].should.equal(0);
+      it('Simon should be done animating the tile matching the previous color', function() {
+        page.find('#blue').hasClass('flash').should.equal(false);
+      });
+
+      it('Simon should replay the next color in his sequence', function() {
+        game.replayColorAt.callCount.should.equal(2);
+        expect(game.replayColorAt.secondCall.args[0]).to.equal(1);
+      });
+
+      it('Simon should animate the tile matching the next color', function() {
+        page.find('#red').hasClass('flash').should.equal(true);
+      });
     });
   });
 });
